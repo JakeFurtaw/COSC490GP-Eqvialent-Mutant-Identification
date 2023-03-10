65a5c5b0c5b7f6a8b461e443d1e1245fdd661fa1
Brandon82 <83241992+Brandon82@users.noreply.github.com>
2023-03-10 17:22:14 -0500

    Converted operator to list to support storing multiple operators.

--- Mutant.py
+++ Mutant.py
@@ -4,10 +4,10 @@ class Mutant:
     def __init__(self, id=None, operator=None, equivalence=None, program=None, difference=None):
         # Each of these represent the values of the dataset
         self.id = id
-        self.operator = operator
+        self.operator = operator if operator is not None else []
         self.equivalence = equivalence
         self.program = program
         self.difference = difference
 
-    def get_mutated_coded(self):
+    def get_mutated_code(self):
         return 
--- data_parser.py
+++ data_parser.py
@@ -2,13 +2,14 @@ import rdflib
 import json
 from globals import *
 from Mutant import Mutant
+import javalang
 
 g = rdflib.Graph()
 g.parse("./dataset/dataset.ttl", format="turtle")
 serialized_json = g.serialize(format='json-ld', indent=4)
 
 # To see what the JSON object looks like:
-# print(serialized_json)
+print(serialized_json)
 
 # Convert JSON to a Python dictionary
 json_dict = json.loads(serialized_json)
@@ -25,12 +26,13 @@ for element in json_dict:
         print("Program key not found")
 
     if operator_key in element:
-        operator = element[operator_key][0]['@id']
-        operator_cleaned = operator.replace("mb:operator#", "")
-        mutant.operator = operator_cleaned
-        print(f"OPERATOR: {mutant.operator}")
-    else:
-        print("Operator key not found")
+        operators_list = element[operator_key]
+        for operators in operators_list:
+            operator_ids = operators['@id'].split(",")
+            for operator_id in operator_ids:
+                operator_cleaned = operator_id.replace("mb:operator#", "").strip()
+                mutant.operator.append(operator_cleaned)
+        print(f"OPERATORS: {mutant.operator}")
 
     if equivalence_key in element:
         equivalence = element[equivalence_key][0].get('@value')
@@ -39,8 +41,7 @@ for element in json_dict:
     else:
         print("Equivalence key not found")
 
-    id_val = element.get('@id')
-    if id_val:
+    if id_val := element.get('@id'):
         id_val_cleaned = id_val.replace("mb:mutant#", "")
         mutant.id = id_val_cleaned
         print(f"ID: {mutant.id}")
